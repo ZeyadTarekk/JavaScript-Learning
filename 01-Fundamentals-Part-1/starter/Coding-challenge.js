210277307e4bc334f17367b80ea2465cdd9d4661
Zeyad <zeyad.ta01@gmail.com>
2021-10-07 21:34:54 +0200

    Coding Challenge 1

---
+++ 01-Fundamentals-Part-1/starter/Coding-challenge.js
@@ -0,0 +1,33 @@
+let markMass = 78;
+let markHeight = 1.69;
+
+let markBMI = markMass / markHeight ** 2;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = false;
+if (markBMI > johnBMI) markHigherBMI = true;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHigherBMI);
+
+markMass = 95;
+markHeight = 1.88;
+
+markBMI = markMass / markHeight ** 2;
+
+johnMass = 85;
+johnHeight = 1.76;
+
+johnBMI = johnMass / johnHeight ** 2;
+
+markHigherBMI = false;
+if (markBMI > johnBMI) markHigherBMI = true;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHigherBMI);
