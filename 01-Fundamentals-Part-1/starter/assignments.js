542cf22310294c01e9479be5d16efe05820e97e2
Zeyad <zeyad.ta01@gmail.com>
2021-10-08 16:08:20 +0200

    assignments

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -34,3 +34,15 @@ if (population > 33) {
 } else {
   console.log(`${country}'s population is ${33 - population} below average '`);
 }
+
+let numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
