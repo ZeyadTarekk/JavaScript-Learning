af24d6d8dc8e7cf722df7a13c709c62573e8fd80
Zeyad <zeyad.ta01@gmail.com>
2021-10-09 23:35:05 +0200

    Coding Challenge

--- 01-Fundamentals-Part-1/starter/Coding-challenge.js
+++ 01-Fundamentals-Part-1/starter/Coding-challenge.js
@@ -1,59 +1,67 @@
-// let markMass = 78;
-// let markHeight = 1.69;
+// // let markMass = 78;
+// // let markHeight = 1.69;
 
-// let markBMI = markMass / markHeight ** 2;
+// // let markBMI = markMass / markHeight ** 2;
 
-// let johnMass = 92;
-// let johnHeight = 1.95;
+// // let johnMass = 92;
+// // let johnHeight = 1.95;
 
-// let johnBMI = johnMass / johnHeight ** 2;
+// // let johnBMI = johnMass / johnHeight ** 2;
 
-// let markHigherBMI = false;
-// if (markBMI > johnBMI) markHigherBMI = true;
+// // let markHigherBMI = false;
+// // if (markBMI > johnBMI) markHigherBMI = true;
 
-// console.log(markBMI);
-// console.log(johnBMI);
-// console.log(markHigherBMI);
+// // console.log(markBMI);
+// // console.log(johnBMI);
+// // console.log(markHigherBMI);
 
-// if (markHigherBMI) {
-//   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
-// } else {
-//   console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
-// }
+// // if (markHigherBMI) {
+// //   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+// // } else {
+// //   console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+// // }
 
-// markMass = 95;
-// markHeight = 1.88;
+// // markMass = 95;
+// // markHeight = 1.88;
 
-// markBMI = markMass / markHeight ** 2;
+// // markBMI = markMass / markHeight ** 2;
 
-// johnMass = 85;
-// johnHeight = 1.76;
+// // johnMass = 85;
+// // johnHeight = 1.76;
 
-// johnBMI = johnMass / johnHeight ** 2;
+// // johnBMI = johnMass / johnHeight ** 2;
 
-// markHigherBMI = markBMI > johnBMI;
+// // markHigherBMI = markBMI > johnBMI;
 
-// console.log(markBMI);
-// console.log(johnBMI);
-// console.log(markHigherBMI);
+// // console.log(markBMI);
+// // console.log(johnBMI);
+// // console.log(markHigherBMI);
 
-// let newString = `Mark BMI = ${markBMI} and John BMI = ${johnBMI} `;
-// console.log(newString);
+// // let newString = `Mark BMI = ${markBMI} and John BMI = ${johnBMI} `;
+// // console.log(newString);
 
-// if (markHigherBMI) {
-//   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
-// } else {
-//   console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
-// }
+// // if (markHigherBMI) {
+// //   console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+// // } else {
+// //   console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+// // }
 
-const DolphinsAVG = (97 + 112 + 101) / 3;
-const KoalasAVG = (109 + 95 + 106) / 3;
+// const DolphinsAVG = (97 + 112 + 101) / 3;
+// const KoalasAVG = (109 + 95 + 106) / 3;
 
-if (DolphinsAVG > KoalasAVG) console.log("Dolphins WON!");
-else if (DolphinsAVG < KoalasAVG) console.log("Koalas WON!");
-else console.log("DRAW!!");
+// if (DolphinsAVG > KoalasAVG) console.log("Dolphins WON!");
+// else if (DolphinsAVG < KoalasAVG) console.log("Koalas WON!");
+// else console.log("DRAW!!");
 
-if (DolphinsAVG > KoalasAVG && DolphinsAVG > 100) console.log("Dolphins WON!");
-else if (DolphinsAVG < KoalasAVG && KoalasAVG > 100) console.log("Koalas WON!");
-else if (DolphinsAVG === KoalasAVG && KoalasAVG > 100) console.log("DRAW!!");
-else console.log("NO ONE WON");
+// if (DolphinsAVG > KoalasAVG && DolphinsAVG > 100) console.log("Dolphins WON!");
+// else if (DolphinsAVG < KoalasAVG && KoalasAVG > 100) console.log("Koalas WON!");
+// else if (DolphinsAVG === KoalasAVG && KoalasAVG > 100) console.log("DRAW!!");
+// else console.log("NO ONE WON");
+
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+console.log(tip);
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
