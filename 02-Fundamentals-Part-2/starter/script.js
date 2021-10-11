febf2f0bc811119ab654f90aec2a44299a51da96
Zeyad <zeyad.ta01@gmail.com>
2021-10-12 00:49:24 +0200

    Strict mode reserved words

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,3 +6,8 @@ const passTest = true;
 if (passTest) hasDriversLi = true;
 
 if (hasDriversLic) console.log("I Have lic");
+
+// and also reserved words that may be used in the furure
+
+const interface = 123;
+console.log(interface);
