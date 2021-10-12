37b97fbf461191c2e191e1feb2456f735a801495
Zeyad <zeyad.ta01@gmail.com>
2021-10-13 01:17:04 +0200

    Function Expressions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -25,3 +25,18 @@ function zeyad(z1, z2) {
 
 let funcTest = zeyad("ZE", 3);
 console.log(funcTest);
+
+console.log(getAge1(2001));
+
+//Function declaration
+function getAge1(birthYear) {
+  return 2021 - birthYear;
+}
+
+//Function Expression
+
+const getAge2 = function (birthYear) {
+  return 2021 - birthYear;
+};
+console.log(getAge2(2000));
+// console.log(getAge3(1999));
