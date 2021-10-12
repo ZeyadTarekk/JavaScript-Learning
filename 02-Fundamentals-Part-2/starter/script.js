58c2d68733e3a50af9514611d72ff84942ba9b43
Zeyad <zeyad.ta01@gmail.com>
2021-10-12 21:35:37 +0200

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,27 @@
 "use strict";
 
-let hasDriversLic = false;
-const passTest = true;
+// let hasDriversLic = false;
+// const passTest = true;
 
-if (passTest) hasDriversLi = true;
+// if (passTest) hasDriversLi = true;
 
-if (hasDriversLic) console.log("I Have lic");
+// if (hasDriversLic) console.log("I Have lic");
 
-// and also reserved words that may be used in the furure
+// // and also reserved words that may be used in the furure
 
-const interface = 123;
-console.log(interface);
+// const interface = 123;
+// console.log(interface);
+
+function logger() {
+  console.log("I'm Zeyad Tarek");
+}
+
+logger();
+logger();
+
+function zeyad(z1, z2) {
+  return z1 + z2;
+}
+
+let funcTest = zeyad("ZE", 3);
+console.log(funcTest);
