45c78345b1a531379b6552e2e58bb36006e8540b
Zeyad <zeyad.ta01@gmail.com>
2021-10-14 00:15:44 +0200

    Assignments

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -77,3 +77,21 @@ function describeCountry(country1, population1, capitalCity) {
 }
 
 console.log(describeCountry(country, population, capital));
+
+function percentageOfWorld1(popul) {
+  return (popul / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (popula) {
+  return (popul / 7900) * 100;
+};
+
+const pop1 = percentageOfWorld1(1441);
+const pop2 = percentageOfWorld1(100);
+const pop3 = percentageOfWorld1(2541);
+console.log(pop1, pop2, pop3);
+
+const pop4 = percentageOfWorld1(1441);
+const pop5 = percentageOfWorld1(100);
+const pop6 = percentageOfWorld1(2541);
+console.log(pop4, pop5, pop6);
