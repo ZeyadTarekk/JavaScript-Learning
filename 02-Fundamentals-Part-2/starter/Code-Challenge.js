e8e8aa500846f2bed021373b32377b5d7acb04a6
Zeyad <zeyad.ta01@gmail.com>
2021-10-16 01:01:52 +0200

    Coding Challenge

--- 02-Fundamentals-Part-2/starter/Code-Challenge.js
+++ 02-Fundamentals-Part-2/starter/Code-Challenge.js
@@ -21,3 +21,19 @@ avgDol = calcAverage(23, 34, 27);
 
 let secondCheck = checkWinner(avgDol, avgKoa);
 console.log(secondCheck);
+
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) return bill * 0.15;
+  else return bill * 0.2;
+}
+
+// console.log(calcTip(400));
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(total);
