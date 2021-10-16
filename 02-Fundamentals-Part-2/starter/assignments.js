cfc704f8819310a2ee31ce6f387338f8ec6f6c28
Zeyad <zeyad.ta01@gmail.com>
2021-10-17 00:03:58 +0200

    Assignments

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -185,3 +185,15 @@ for (let i = 0; i < populations.length; i++) {
 console.log(percentages2);
 
 console.log(percentages);
+
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
