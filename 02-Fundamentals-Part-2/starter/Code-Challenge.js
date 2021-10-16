2a8d6d74a22718894c5eb38f27d1386e46547cdc
Zeyad <zeyad.ta01@gmail.com>
2021-10-16 22:06:02 +0200

    Coding Challenge

--- 02-Fundamentals-Part-2/starter/Code-Challenge.js
+++ 02-Fundamentals-Part-2/starter/Code-Challenge.js
@@ -37,3 +37,33 @@ const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills);
 console.log(tips);
 console.log(total);
+
+const mark = {
+  fullName: "Mark Miller",
+  weight: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.weight / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+const john = {
+  fullName: "Johm Smith",
+  weight: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.weight / this.height ** 2;
+    return this.BMI;
+  },
+};
+
+if (mark.calcBMI() > john.calcBMI()) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s (${john.BMI})!"`
+  );
+} else {
+  console.log(
+    `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s (${mark.BMI})!"`
+  );
+}
