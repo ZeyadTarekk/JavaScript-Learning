b100b2af7752dd1e24d1677453751349732577b2
Zeyad <zeyad.ta01@gmail.com>
2021-10-17 04:41:13 +0200

    Coding Challenge

--- 03-Developer-Skills/starter/code-challenge.js
+++ 03-Developer-Skills/starter/code-challenge.js
@@ -3,8 +3,11 @@
 //17ºC in 1
 // days ... 21ºC in 2 days ... 23ºC in 3 days ...
 function printForecast(arr) {
+  let newString = "";
   for (let i = 0; i < arr.length; i++)
-    console.log(`${arr[i]}ºC in ${i + 1} days`);
+    newString += ` ... ${arr[i]}ºC in ${i + 1} days`;
+  newString += " ...";
+  console.log(newString);
 }
 
 printForecast([12, 5, -5, 0, 4]);
