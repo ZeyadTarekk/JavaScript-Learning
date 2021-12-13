2ad7177c606a47276bde4c70291b6d0614fb74a7
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-14 01:51:23 +0200

    fetch then promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -34,7 +34,7 @@ const countriesContainer = document.querySelector(".countries");
 //   });
 // };
 
-const renderCounrty = function (data, className = "") {
+const renderCountry = function (data, className = "") {
   const html = `
   <article class="country ${className}">
   <img class="country__img" src="${data.flag}" />
@@ -100,6 +100,13 @@ const renderCounrty = function (data, className = "") {
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(res => res.json())
-    .then(data => renderCounrty(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(res2 => res2.json())
+    .then(data2 => renderCountry(data2, "neighbour"));
 };
 getCountryData("portugal");
