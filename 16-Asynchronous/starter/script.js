2f59d02a08c911467fe36a14f1e53d8676669f61
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-14 16:15:33 +0200

    geoapify api

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,11 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest();
@@ -99,16 +104,62 @@ const renderCountry = function (data, className = "") {
 // };
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      // if (!res.ok) {
+      //   throw new Error(`Country Not found (${res.status})`);
+      // }
+      return res.json();
+    })
     .then(data => {
+      // console.log(data[0]);
       renderCountry(data[0]);
+      if (!data[0].borders) throw new Error("No Neighbours");
       const neighbour = data[0].borders[0];
-      if (!neighbour) return;
+      // const neighbour = "asd";
+      // console.log(neighbour);
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
-    .then(res2 => res2.json())
-    .then(data2 => renderCountry(data2, "neighbour"));
+    .then(res2 => {
+      console.log(res2);
+      return res2.json();
+    })
+    .then(data2 => renderCountry(data2, "neighbour"))
+    .catch(err => {
+      console.error(`${err}💥💥💥`);
+      renderError(`Something Went Wrong ${err.message} Try Again`);
+    });
 };
 btn.addEventListener("click", function () {
   getCountryData("portugal");
 });
+// getCountryData("asd");
+// getCountryData("portugal");
+const whereAmI = function (lat, lng) {
+  fetch(
+    `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&format=json&apiKey=a15434b551ad4fc48c638920b4640e0e`
+  )
+    .then(Response => {
+      console.log(Response);
+      return Response.json();
+    })
+    .then(data => {
+      console.log(data.results[0].country);
+      if (data.results[0].country) {
+        const myCount = data.results[0].country;
+        getCountryData(myCount);
+      } else {
+        throw new Error("No Such Country");
+      }
+    })
+    .catch(err => {
+      console.log(err.message);
+      renderError(err.message);
+    });
+};
+// console.log(whereAmI(52.508, 13.381));
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+// console.log(country);
+// getCountryData(country);
