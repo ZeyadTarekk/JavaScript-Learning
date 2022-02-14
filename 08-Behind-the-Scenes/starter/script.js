55b3debb79fa62f72fb1fbe48c0ae41c0b0b5c29
Zeyad Tarek <Zeyad.ta01@gmail.com>
2022-02-14 02:17:57 +0200

    Scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
-'use strict';
+"use strict";
+
+if (true) {
+  var fName = "zeyad";
+}
+
+console.log(fName); // will work if it is var only because var is not block scoped but if it is let or const it will give referrence error
+
+// function calcAge(birthYear) {
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  // console.log(firstName);
+  console.log("Hello");
+  return age;
+}
+calcAge(2001);
+let firstName = "Zeyaddddd";
