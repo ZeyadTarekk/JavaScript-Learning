a0c7ff7d0e3cb53833af8bbd3f2bee75514c9213
Zeyad Tarek <Zeyad.ta01@gmail.com>
2022-02-18 17:55:02 +0200

    Spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
 const restaurant = {
-  names: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  names: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -39,8 +39,20 @@ a = 6;
 console.log(a);
 console.log(arr);
 
-let [myOrder1, myOrder2] = restaurant.order(1,2);
-console.log(myOrder1,myOrder2); 
+let [myOrder1, myOrder2] = restaurant.order(1, 2);
+console.log(myOrder1, myOrder2);
 
-const {names , mainMenu} = restaurant;
+const { names, mainMenu } = restaurant;
 console.log(names, mainMenu);
+
+const arrCopy = [...restaurant.mainMenu];
+console.log(arrCopy);
+console.log(restaurant.mainMenu);
+
+arrCopy[0] = "Zeyad";
+console.log(arrCopy);
+console.log(restaurant.mainMenu);
+
+let myName = "Zeyad";
+const myNameArr = [...myName];
+console.log(myNameArr);
