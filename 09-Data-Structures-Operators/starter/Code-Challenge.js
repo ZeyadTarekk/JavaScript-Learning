e4c1dfb8a9748eb35e46d85a5f70abfb1397e366
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-03-15 00:56:18 +0200

    Code challnege 1

---
+++ 09-Data-Structures-Operators/starter/Code-Challenge.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+const [gk1, ...fieldPlayers1] = players1;
+console.log(gk1, fieldPlayers1);
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+console.log(players1Final);
+const { team1, x: Draw, team2 } = { ...game.odds };
+console.log(team1, Draw, team2);
+
+function printGoals(...playersNames) {
+  for (let i = 0; i < playersNames.length; i++) {
+    console.log(`${playersNames[i]} : ${playersNames.length}`);
+  }
+}
+
+// printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
+printGoals(...game.scored);
