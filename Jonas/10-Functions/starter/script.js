8b07c69a8dd053644ba76efb0d8e7e9873a5478c
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-07 10:50:10 +0200

    functions accpeting callback

--- Jonas/10-Functions/starter/script.js
+++ Jonas/10-Functions/starter/script.js
@@ -27,3 +27,27 @@ createBooking("LH123", 5);
 createBooking("LH123", undefined, 1000);
 
 console.log(typeof createBooking);
+
+const oneWord = function (str) {
+  return str.replaceAll(" ", "").toLowerCase();
+};
+
+// console.log(oneWord("Zeyad Tarek Khairy"));
+
+const upperFirstWord = function (str) {
+  const [firstWord, ...others] = str.split(" ");
+  return firstWord.toUpperCase() + " " + others.join(" ");
+};
+
+// console.log(upperFirstWord("Zeyad Tarek Khairy"));
+
+// Higher order Function
+const transformer = function (str, fn) {
+  console.log(`Before transformation: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("Javascript is the best ever", upperFirstWord);
+// console.log(transformer("Javascript is the best ever", upperFirstWord));
+transformer("Javascript is the best ever", oneWord);
