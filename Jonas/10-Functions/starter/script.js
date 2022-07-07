a13b954ddd6d5b725771fbcf1842642d4d93a0d5
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-07 09:42:12 +0200

    default parametrs for functions

--- Jonas/10-Functions/starter/script.js
+++ Jonas/10-Functions/starter/script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 2);
+createBooking("LH123", 5);
+
+createBooking("LH123", undefined, 1000);
