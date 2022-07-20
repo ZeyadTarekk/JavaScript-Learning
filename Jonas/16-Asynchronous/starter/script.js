9d043c41e268aef7f7f955c468eb20e3e5d474f6
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-20 17:32:33 +0200

    AJAX call

--- Jonas/16-Asynchronous/starter/script.js
+++ Jonas/16-Asynchronous/starter/script.js
@@ -2,6 +2,44 @@
 
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
+////////////////////////////////////////////////////
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    console.log(request.responseText);
+
+    const [data] = JSON.parse(request.responseText);
+    console.log(data);
+
+    const language = Object.values(data.languages)[0];
+
+    const currency = Object.values(data.currencies)[0].name;
+
+    const card = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        data.population / 1000000
+      ).toFixed(2)}M people</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
+    </div>
+  </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML("beforeend", card);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData("egypt");
+getCountryData("usa");
 
 /*
 const renderError = function (msg) {
