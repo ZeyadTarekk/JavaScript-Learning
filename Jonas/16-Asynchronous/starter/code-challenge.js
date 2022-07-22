a0ef70763f55fba7305e7b513eefdf7ea21a01f9
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-22 16:19:07 +0200

    add more than city

--- Jonas/16-Asynchronous/starter/code-challenge.js
+++ Jonas/16-Asynchronous/starter/code-challenge.js
@@ -90,4 +90,7 @@ const whereAmI = function (lat, long) {
       countriesContainer.style.opacity = 1;
     });
 };
-whereAmI(-3321323.933, 1821312.474);
+// whereAmI(-3321323.933, 1821312.474);
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
