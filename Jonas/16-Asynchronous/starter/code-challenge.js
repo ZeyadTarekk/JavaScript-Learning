391081c3b574c3419d3d1f15ba0fe839e2b4f90d
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-07-23 18:19:15 +0200

    code challenge #2

--- Jonas/16-Asynchronous/starter/code-challenge.js
+++ Jonas/16-Asynchronous/starter/code-challenge.js
@@ -111,3 +111,47 @@ const whereAmI = function () {
 // whereAmI(-33.933, 18.474);
 
 btn.addEventListener("click", whereAmI);
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector(".images");
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = imgPath;
+
+    img.addEventListener("load", function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+};
+
+let currentImg;
+createImage("./img/img-1.jpg")
+  .then((img) => {
+    currentImg = img;
+    console.log("image 1 loaded");
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+    return createImage("./img/img-2.jpg");
+  })
+  .then((img) => {
+    currentImg = img;
+    console.log("Image 2 loaded");
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = "none";
+  })
+  .catch((err) => console.log(err.message));
